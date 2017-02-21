f0b7c53734f752ddcb43cbdee3f8294cd9f48a90
Matthew Balkaran <bmatthewbalkaran@gmail.com>
2017-02-21 14:45:55 -0500

    Pushing Hello World program. (First ICS3U Program)

---
+++ Hello/src/balkaran/HelloWorld.java
@@ -0,0 +1,16 @@
+package balkaran;
+
+/**
+ * HelloWorld.java
+ * The program prints Hello, World! to the console.
+ * 21/02/2017
+ * @author Matthew Balkaran
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
