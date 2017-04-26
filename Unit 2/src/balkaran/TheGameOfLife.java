acd7280458623568a7a96cf340d4cf12dfbe0c5c
Matthew B <Matthew B@DESKTOP-9FN5PFL>
2017-04-26 15:03:17 -0400

    Pushing TheGameOfLife.java
    Completed Position Rules (method)
    TBD: continue loops & game overs

--- Unit 2/src/balkaran/TheGameOfLife.java
+++ Unit 2/src/balkaran/TheGameOfLife.java
@@ -37,79 +37,338 @@ public class TheGameOfLife {
 			}
 			System.out.println();
 		}
-		System.out.println("Is this okay as the starting grid?");
+		String userContinue = sc.nextLine();
+		boolean test;
+		System.out.println();
+		System.out.println("Would you like to continue? (Y/N");
+		for (int r = 0; r < 20; r++) {
+			for (int c = 0; c < 20; c++) {
+				System.out.print(grid[r][c] = positionRules(grid, r, c));
+			}
+			System.out.println();
+		}
 	}
-	public static char position (char[][] grid, int r, int c) {
-		int total = 0;
-		if (grid[r][c] == 'X') {
-			if (grid[r][c] == grid[0][0]){ //if cell is in the top left corner
 
+	public static char positionRules (char[][] grid, int r, int c) {
+		int total = 0;
+		if (grid[r][c] == 'X') { //if cell is initially alive
+			if (grid[r][c] == grid[0][0]){ //if cell is in the top left corner
+				if (grid[0][1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[1][1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[1][0] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[19][0]){ //if cell is in the top right corner
-
+				if (grid[19][1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[18][1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[18][0] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[19][19]){ //if cell is in the bottom right corner
-
+				if (grid[18][18] == 'X') {
+					total = total + 1;
+				}
+				if (grid[18][19] == 'X') {
+					total = total + 1;
+				}
+				if (grid[19][18] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[0][19]){ //if cell is in the bottom left corner
-
+				if (grid[1][18] == 'X') {
+					total = total + 1;
+				}
+				if (grid[1][19] == 'X') {
+					total = total + 1;
+				}
+				if (grid[0][18] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[r][0]){ //if cell is in top edge
-
+				if (grid[r][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[r][19]){ //if cell is in bottom edge
-
+				if (grid[r][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[0][c]){ //if cell is in left edge
-
+				if (grid[r][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[19][c]){ //if cell is in right edge
-
+				if (grid[r][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 2 | total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
-			else {
-				for (int x = 0; x < 3; x++) {
-					for (int y = 0; y < 3; y++) {
-						if (grid[r + x][c + x] == 'X' && (x != 1 && y != 1)) {
+			else { //if cell is in center position (no edges)
+				for (int x = -1; x < 2; x++) {
+					for (int y = -1; y < 2; y++) {
+						if (grid[r + x][c + x] == 'X' && (x != 0 && y != 0)) {
 							total = total + 1;
 						}
 					}
 				}
 				if (total == 2 || total == 3) {
-					grid[r][c] = 'X';
+					return 'X';
 				}
 				else {
-					grid[r][c] = '0';
+					return '0';
 				}
 			}
 		}
-		else if (grid[r][c] == '0') {
+		else if (grid[r][c] == '0') { //if cell is initially dead
 			if (grid[r][c] == grid[0][0]){ //if cell is in the top left corner
-
+				if (grid[0][1] == 'X' && grid[1][1] == 'X' && grid[1][0] == 'X') {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[19][0]){ //if cell is in the top right corner
-
+				if (grid[19][1] == 'X' && grid[18][1] == 'X' && grid[18][0] == 'X') {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[19][19]){ //if cell is in the bottom right corner
-
+				if (grid[18][18] == 'X' && grid[18][19] == 'X' && grid[19][18] == 'X') {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[0][19]){ //if cell is in the bottom left corner
-
+				if (grid[0][18] == 'X' && grid[1][19] == 'X' && grid[1][18] == 'X') {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[r][0]){ //if cell is in top edge
-
+				if (grid[r][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[r][19]){ //if cell is in bottom edge
-
+				if (grid[r][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[0][c]){ //if cell is in left edge
-
+				if (grid[r][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r + 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 			else if (grid[r][c] == grid[19][c]){ //if cell is in right edge
-
+				if (grid[r][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c - 1] == 'X') {
+					total = total + 1;
+				}
+				if (grid[r - 1][c + 1] == 'X') {
+					total = total + 1;
+				}
+				if (total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
-			else {
-
+			else { //if cell is in center position (no edges)
+				for (int x = -1; x < 2; x++) {
+					for (int y = -1; y < 2; y++) {
+						if (grid[r + x][c + x] == 'X' && (x != 0 && y != 0)) {
+							total = total + 1;
+						}
+					}
+				}
+				if (total == 3) {
+					return 'X';
+				}
+				else {
+					return '0';
+				}
 			}
 		}
+		return 'W'; //Meant to stand out for error checking
 	}
 }
